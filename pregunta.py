51debb076bf6b9fdb465c2cc8a6c49bf8995e813
jromog11 <jromog@unal.edu.co>
2024-03-30 16:16:23 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,26 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
-
 def ingest_data():
+    ruta_archivo = "clusters_report.txt"
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf(
+        ruta_archivo,
+        colspecs="infer",
+        widths=[9,16,16,80],
+        header = None,
+        names = ["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"], 
+        converters = {"porcentaje_de_palabras_clave": lambda x: x.rstrip(" %").replace(",", ".")}
+    ).drop(index={0,1,2}).ffill()
 
-    return df
+    df["cluster"] = pd.to_numeric(df["cluster"])
+    df['cantidad_de_palabras_clave'] = pd.to_numeric(df['cantidad_de_palabras_clave'])
+    df['porcentaje_de_palabras_clave'] = pd.to_numeric(df['porcentaje_de_palabras_clave'])
+
+    concatenated_column = df.groupby(['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave'])['principales_palabras_clave'].apply(lambda x: ' '.join(x)).reset_index()
+
+    concatenated_column['principales_palabras_clave'] = concatenated_column['principales_palabras_clave'].replace(r'\s+', ' ', regex=True)
+    concatenated_column["principales_palabras_clave"] = concatenated_column["principales_palabras_clave"].apply(lambda x : x.replace(".", ""))
+
+    return concatenated_column
+
